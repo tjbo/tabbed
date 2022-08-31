dd35f998ec62b6f83bd069b600681297a86f6518
tjbo <tom@prototypable.ca>
2022-08-31 16:14:37 -0500

    update padding on tabs

--- tabbed.c
+++ tabbed.c
@@ -984,7 +984,7 @@ setup(void)
 	screen = DefaultScreen(dpy);
 	root = RootWindow(dpy, screen);
 	initfont(font);
-	bh = dc.h = dc.font.height + 2;
+	bh = dc.h = dc.font.height + 10;
 
 	/* init atoms */
 	wmatom[WMDelete] = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
