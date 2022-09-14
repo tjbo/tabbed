028f0bf75b395b2716743dceeab7bdb156aa6edf
tjbo <tom@prototypable.ca>
2022-09-14 12:00:18 -0500

    change next and prev tab shortcuts

--- config.h
+++ config.h
@@ -38,13 +38,11 @@ static Key keys[] = {
 	/* modifier             key        function     argument */
 	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
 	// { MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
-
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_j,      rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_k,      rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_h,      movetab,     { .i = -1 } },
+	// { MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
-
 	// { MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
 	{ MODKEY,               XK_2,      move,        { .i = 1 } },
@@ -56,13 +54,9 @@ static Key keys[] = {
 	{ MODKEY,               XK_8,      move,        { .i = 7 } },
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
-
 	{ MODKEY,               XK_w,      killclient,  { 0 } },
 	{ MODKEY,               XK_q,      killclient,  { 0 } },
-
-
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
-
 	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 };
